b4a27f13942f4027d77c671b52c32e03c16f9941
Jonas Rauber <git@jonasrauber.de>
2015-08-14 14:31:21 +0200

    added more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using DiscreteEntropyEstimators
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@test entropy_ml(Frequencies([1, 1, 2])) == entropy(MLEE(), Frequencies([1, 1, 2]))
+@test entropy_ml(Frequencies([1, 1, 2]), 2) == 1.5
+@test entropy_ml(Frequencies([1, 1, 2]), e) == entropy_ml(Frequencies([1, 1, 2]))
+@test entropy_ml(Frequencies([1, 1, 1, 1, 4, 4, 4]), 4) == 1.25
+@test entropy_ml(Samples([1, 2, 3, 3])) == entropy_ml(Frequencies([1, 1, 2]))
